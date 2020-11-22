7bf0368ea361b305a204e6dcc9ec270e8e3f480f
AstroFloyd <AstroFloyd@gmail.com>
2020-11-22 14:25:24 +0100

    gfortran: add -Wno-function-elimination compiler flag.

--- CMake/CompilerFlags_Fortran.cmake
+++ CMake/CompilerFlags_Fortran.cmake
@@ -64,7 +64,7 @@ if( Fortran_COMPILER_NAME MATCHES "gfortran" )
   endif( WANT_CHECKS )
   
   if( WANT_WARNINGS )
-    set( WARN_FLAGS "-Wall -Wextra" )
+    set( WARN_FLAGS "-Wall -Wextra -Wno-function-elimination" )
   endif( WANT_WARNINGS )
   if( STOP_ON_WARNING )
     set( WARN_FLAGS "${WARN_FLAGS} -Werror" )
